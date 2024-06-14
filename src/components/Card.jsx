1d26841f635af844b860eae03dddcf895d9bf50c
RoyaEhabAdel <137731652+RoyaEhabAdel@users.noreply.github.com>
2024-06-14 17:20:31 +0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,36 +6,39 @@ import axios from 'axios';
 
 const Card = () => {
   const [resData,setResData] = useState([]);
+  const [Total, setTotal] = useState(1)
 
-  const items = 4;
+  const items = 3;
   const [current,setCurrent] = useState(1);
-  const NbPage = Math.ceil(resData.length / items);
   const startIndex = (current - 1) * items;
   const endIndex = startIndex + items
 
-  const DataPerPage = resData.slice(startIndex,endIndex)
 
   useEffect(() =>{
   const getData = async () => {
       try{
-      const response = await axios.get('https://dummyjson.com/recipes');
+      const response = await axios.get(`https://dummyjson.com/recipes?limit=${items}&skip=${startIndex}`);
       const recipes = response.data.recipes;
+      const total = Math.ceil( response.data.total / items)
+      setTotal(total)
       setResData(recipes)
       console.log(recipes)
+      
       } catch(err) {
           console.log(err)
       }
   }
   getData()
-}, []);
+}, [startIndex,items]);
     
+console.log(current)
   return (
     <div>
         <div className='heading'>
             <h1>Dummy Recepies</h1>
         </div>
         <div className='container'>
-        { resData && DataPerPage.map(item => {
+        { resData && resData.length>0 && resData.map(item => {
             return(
             <CardInfo item={item} />
             )
@@ -44,9 +47,11 @@ const Card = () => {
             </div>
             <div className='btns'>
               {
-                Array.from({length:NbPage}, (_, i)=> i+1).map(page=>{
-                  return <button onClick={()=> setCurrent(page)}>{page}</button>
-                })
+                Array.from({length:Total}, (_, i)=> i+1).map(page=>(
+                  <button key={page} onClick={() => setCurrent(page)} disabled={page === current}>
+                  {page}
+                  </button>
+                ))
               }
             </div>
  
